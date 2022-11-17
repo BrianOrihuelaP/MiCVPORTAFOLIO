8eeff31ff2acdcb30d255e271f756c1163771bbc
Brian U. Orihuela P <89498777+BrianOrihuelaP@users.noreply.github.com>
2022-11-16 18:44:44 -0600

    add portfolio v1.0
    Agregando portafolio web version 1.0 completamente funcional

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Programando la animacion de escritura
+var typed = new Typed(".typing",{
+    strings:["Desarrollador de software","Desarrollador web"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+});
+
+
